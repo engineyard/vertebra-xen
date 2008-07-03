e9db46444a0e16266a72098cc60234f806cfb6fb
Joshua Sierles <jsierles@126.Red-88-31-174.staticIP.rima-tde.net>
2008-07-03 12:54:57 +0200

    add various operations

--- lib/vertebra-xen/actor.rb
+++ lib/vertebra-xen/actor.rb
@@ -10,31 +10,50 @@ module VertebraXen
       
       provides '/xen'
 
-      # no arguments
+      # should be able to list running or all slices
     	def list(options = {})
     	  Xm::ListOutput.new.parse(StringIO.new(`xm list`))
     	end
     	
-    	# no arguments
+    	# get slice info
     	def info(options = {})
     	  Xm::InfoOutput.new.parse(StringIO.new(`xm info`))
     	end
     	
-    	# no arguments
+    	# parse entire xenstore_ls output
     	def xenstore_ls(options = {})
     	  Xm::XenstoreLs.new(StringIO.new(`xenstore-ls`)).parse
       end
 
       # takes a slice name, i.e. ey04-s00010
+<<<<<<< HEAD:lib/vertebra-xen/actor.rb
+    	def shutdown_slice(options = {})
+        execute("xm shutdown #{options['slice']}")
+      end
+
+      # takes a slice name, i.e. ey04-s00010
+    	def create_slice(options = {})
+=======
     	def shutdown(options = {})
     	  `xm shutdown #{options['slice']}`
       end
 
       # takes a slice name, i.e. ey04-s00010
     	def create(options = {})
+>>>>>>> 9d6c737c65f6ca4479434ed1f0ae804bc23b31cb:lib/vertebra-xen/actor.rb
     	  `xm create /etc/xen/auto/#{options['slice']}.xen`
       end
+
+      # takes a slice name, i.e. ey04-s00010
+    	def reboot_slice(options = {})
+    	  `xm reboot #{options['slice']}`
+      end
       
+      # takes a slice name, i.e. ey04-s00010      
+      def set_slice_ram(options = {})
+        # use parser to read file, validate ram setting against the maxmem and node, and write back, backup up the original
+      end
+
     end
   end
 end
