c55dcafabccec875beccafa584ed99cbfe5f8712
Joshua Sierles <jsierles@231.Red-88-31-160.staticIP.rima-tde.net>
2008-07-08 23:15:36 +0200

    check properly set memory value and copy original file back

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -8,6 +8,10 @@ describe Xm::ListOutput, "parsing output" do
     @actor = VertebraXen::Actor::Xen.new(options)
     @actor.set_slice_values(options)
     File.exists?(xen_root + "/.ey00-s00348.xen.bak").should == true
+    @next_actor = VertebraXen::Actor::Xen.new(options)
+    conf = XenConfigFile::Parser.new.parse(File.read(File.dirname(__FILE__)+'/fixtures/ey00-s00348.xen')).eval    
+    conf[:memory].value.should == 1024
+    FileUtils.cp(xen_root+"/.ey00-s00348.xen.bak", xen_root+"/ey00-s00348.xen")
   end
 
 end
